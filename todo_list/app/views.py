6d5e1197481aa4653ddc8d0b79e4f58bfdc2510b
weslyramalho <weslyramalho@yahoo.com.br>
2023-10-10 20:12:48 -0300

    views form

--- todo_list/app/views.py
+++ todo_list/app/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import TodoForm
+from .models import Todo
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    item_list = Todo.objects.order_by("-date")
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('app')
+        form = TodoForm()
 
+        page = {
+            "forms": form,
+            "list": item_list,
+            "title": "TODO LIST",
+        }
+        return render(request, 'home.html', page)
+
+def remove(request, item_id):
+    item = Todo.objects.get(id=item_id)
+    item.delete()
+    messages.info(request, "item removed!!")
+    return redirect('app')
